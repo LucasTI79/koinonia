c49595721e1823ccd5fba9f204faeeeb8717ccf1
Lucas Alves <lukasalves271@gmail.com>
2024-07-13 21:06:34 -0300

    feat: show today events

--- src/http/events.ts
+++ src/http/events.ts
@@ -1,10 +1,12 @@
 import { calendar, auth } from "@/lib/googleCalendar";
 
 export async function listEvents(calendarId: string){
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
     return calendar.events.list({
         calendarId,
         maxResults: 10,
         auth,
-        timeMin: new Date().toISOString(),
+        timeMin: today.toISOString(),
     })
 }
