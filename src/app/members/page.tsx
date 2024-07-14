ff75936d48fe45d15cdcb26a4f51d5d542b5b6d4
Lucas Alves <lukasalves271@gmail.com>
2024-07-13 22:20:05 -0300

    feat: back to home

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -5,6 +5,8 @@ import { Footer } from "@/components/footer";
 import { Koinonia } from "@/components/icons/Koinonia";
 import { DailyVerse } from "./components/dailyVerse";
 import { ClientOnly } from "@/components/clientOnly";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 export const revalidate = Number(environments.revalidateTime ||  60 * 5) // 5 minutes
 
@@ -13,13 +15,17 @@ export default function MembersPage(){
 
     return (
         <main className="flex flex-col min-h-screen bg-background">
+            <Link href="/" className="fixed top-8 left-8 flex justify-center items-center gap-2">
+              <ArrowLeft />
+              <p className="hidden md:block">Voltar</p>
+            </Link>
             <section className="pt-40 flex flex-col gap-16 max-w-[1200px] justify-center items-center mx-auto p-4">
                 <div className="flex justify-center items-center">
                     <Koinonia className="hidden md:block dark:fill-white fill-black" width={`6rem`} height={`6rem`} />
                     <h1 className="text-6xl font-black">KOINONIA</h1>
                 </div>
             </section>
-            <section className="pt-24 px-4 w-full max-w-[1200px] mx-auto flex flex-col md:flex-row justify-between gap-8">
+            <section className="pt-24 px-4 w-full max-w-[1200px] mx-auto flex flex-col gap-16 md:flex-row justify-between md:gap-8">
                <div className="w-full md:w-1/2 flex flex-col justify-center items-center md:items-start gap-4">
                     <h2 className="text-3xl font-semibold mb-4">Área de membros</h2>
                     <p>Aqui você poderá encontrar nossa agenda e informações do grupo</p>
@@ -32,7 +38,7 @@ export default function MembersPage(){
                     </ClientOnly>
                </div>
             </section>
-            <section className="pt-24 px-4 w-full max-w-[1200px] mx-auto flex flex-col justify-center md:justify-start gap-8">
+            <section className="pt-40 px-4 w-full max-w-[1200px] mx-auto flex flex-col justify-center md:justify-start gap-8">
                 <h2 className="text-3xl font-semibold mb-4">Próximos eventos</h2>
                 {/* {calendarIds.map((calendarId) => {
                     <CalendarList key={calendarId} calendarId={calendarId}/>
